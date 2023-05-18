4526ee6d8c1d26e7857f02b4c11375ba6dffc68f
Ismael Junior <ismaeljcjunior@gmail.com>
2023-05-17 21:00:59 -0300

    feat PageLogin

--- src/pages/login/PageLogin.tsx
+++ src/pages/login/PageLogin.tsx
@@ -1,3 +1,4 @@
+import { Container, Box, Button, TextField } from '@mui/material';
 
 
 
@@ -5,7 +6,38 @@ export const PageLogin = () => {
 
     return (
         <>
-        PageLogin
+            <Container
+                maxWidth="sm"
+                sx={{
+                    height: '50vh',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    border: '1px solid black', 
+                    backgroundColor: '#f5f5f5', 
+                    borderRadius: '5px'
+                }}
+            >
+                <Box
+                    textAlign="center"
+                    sx={{
+                        width: '100%',
+                        padding: '0 16px',
+                    }}
+                >
+                    <TextField
+                        label="Digite a key"
+                        variant="outlined"
+                        sx={{
+                            marginBottom: '16px',
+                            width: '100%',
+                        }}
+                    />
+                    <Button variant="contained" color="primary">
+                        Clique aqui
+                    </Button>
+                </Box>
+            </Container>
         </>
     )
 
